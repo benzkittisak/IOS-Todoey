824929696d06f84edfc3940de5d2ad55cc529973
benzkittisak <benzkittisak@stv.ac.th>
2022-07-04 16:09:46 +0700

    update model code

--- Todoey/Models/Categories.swift
+++ Todoey/Models/Categories.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import RealmSwift
+
+class Category: Object {
+    @Persisted var name:String = ""
+    
+//    สร้างความสัมพันธ์ของข้อมูลกับตัว Items ล่ะนะ
+    let items = List<Items>()
+}
