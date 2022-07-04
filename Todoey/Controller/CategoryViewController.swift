513eb1ece920b5297746d736866ee8bb01319036
benzkittisak <benzkittisak@stv.ac.th>
2022-07-04 09:25:22 +0700

    create IBOutlet addBtn in CategoryViewController

--- Todoey/Controller/CategoryViewController.swift
+++ Todoey/Controller/CategoryViewController.swift
@@ -15,15 +15,8 @@ class CategoryViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addBtnPressed(_ sender: UIBarButtonItem) {
+        
     }
-    */
-
+    
 }
